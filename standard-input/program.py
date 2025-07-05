03b6dbeb0b66db6d5e5b8ec6d3b935b02fe73f4c
Jule Sall <souleymanesall985@gmail.com>
2025-07-05 08:50:10 +0000

    learning the standard input

---
+++ standard-input/program.py
@@ -0,0 +1,11 @@
+# Prompting user name and printing it to the console
+name = input('Hello, What is your name? ')
+print(name)
+
+# Calculating the area of a rectangle
+length = input('Enter the length of the rectangle: ')
+width = input('Enter the width of the rectangle: ')
+
+area = int(length) * int(width) # Use Typecasting to converting str to int
+print("Area: ", area)
+
