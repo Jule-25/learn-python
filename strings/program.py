fa6589c27c93dcedaa734a83f550ac81d62b23b2
Jule Sall <souleymanesall985@gmail.com>
2025-07-05 08:27:56 +0000

    learning strings

---
+++ strings/program.py
@@ -0,0 +1,19 @@
+greet = "Hello"
+
+# Accessing the first element of a string
+print(greet[0]) # 'H'
+
+# Slicing a string
+print(greet[0:3]) # 'Hel'
+print(greet[0:2]) # 'He'
+
+# Addding two strings
+name = "Jule";
+print(greet + " " + name) # Hello Jule
+
+# Uppercasing a string
+print(name.upper()) # JULE
+
+# Splitting a string
+friends = "Fama,Oumar,Ibou"
+print(friends.split(",")) # ['Fama', 'Oumar', 'Ibou']
