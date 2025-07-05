ab54e9cf299eb02771c16ddeecbc84e5862758b6
Jule Sall <souleymanesall985@gmail.com>
2025-07-05 12:28:47 +0000

    learning if-statements

---
+++ if-statements/program.py
@@ -0,0 +1,12 @@
+grade = int(input('Enter your grade: '))
+
+if grade < 50:
+    print('You failed')
+elif grade >= 50 and grade < 80:
+    print('Good enough')
+else:
+    print('Good Job!')
+
+    
+
+
