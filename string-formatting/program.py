d507ff3de37c982cc08cc94753d31478737a4e98
Jule Sall <souleymanesall985@gmail.com>
2025-07-05 08:59:58 +0000

    learning string formatting

---
+++ string-formatting/program.py
@@ -0,0 +1,15 @@
+num1  = 20.3443
+num2 = 40
+
+# Usual way of printing to the standard console
+print("num 1 is: ", num1, " and num 2 is: ", num2)
+
+# Printing using the FORMAT METHOD
+print('num 1 is: {0} and num 2 is: {1}'.format(num1, num2))
+
+# Formatting the output using the FORMAT METHOD
+print('num 1 is: {0:2f} and num 2 is: {1}'.format(num1, num2))
+
+# Using F-STRINGS
+print(f'num 1 is {num1} and num 2 is {num2}')
+
