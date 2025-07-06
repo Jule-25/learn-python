10a236daf3d6c53196218b1b3d44eee249938fb7
Jule Sall <souleymanesall985@gmail.com>
2025-07-06 08:53:54 +0000

    learning dictionaries

---
+++ dictionaries/program.py
@@ -0,0 +1,18 @@
+def ninja_intro(dictionary):
+    for key, val in dictionary.items():
+        print(f'I am {key} and i am a {val} belt')
+
+ninja_belts = {}
+
+while True:
+    ninja_name = input('Enter name: ')
+    ninja_belt = input('Enter belt color: ')
+    ninja_belts[ninja_name] = ninja_belt
+
+    another = input('Add another? (y/n)')
+    if another == 'y':
+        continue
+    else:
+        break
+
+ninja_intro(ninja_belts)
