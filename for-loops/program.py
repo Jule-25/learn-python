62b553177b17e5e06f5460d2cb192ae19698bd08
Jule Sall <souleymanesall985@gmail.com>
2025-07-06 07:37:45 +0000

    learning for loops

---
+++ for-loops/program.py
@@ -0,0 +1,12 @@
+names = ['Oumar', 'Ibou', 'Sarah']
+
+# Iterate through the individual elements of names and print them 
+for name in names:
+    print(name)
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+
+# Print only even numbers
+for num in nums:
+    if num % 2 == 0:
+        print(num)
