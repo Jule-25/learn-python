b6a53e9e37998c545ee70075607ae29e407b4a48
Jule Sall <souleymanesall985@gmail.com>
2025-07-07 08:26:40 +0000

    removing the modules folder as well

--- modules/program.py
+++
@@ -1,7 +0,0 @@
-from packages.planet import Planet
-from packages.cal import planet_mass, planet_vol
-
-naboo = Planet('Naboo', 3000000.0, 300, 'Naboo System')
-naboo_vol = planet_vol(naboo.radius)
-
-print(f"{naboo.name} has a vol of {naboo_vol}")
